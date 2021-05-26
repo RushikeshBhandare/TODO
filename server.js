3527f88b640c374d2873318dac908d57c1e257c7
RushikeshBhandare <rushi5049@gmail.com>
2021-05-27 01:35:44 +0530

    Change in Express.Static File path

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/notes', routerNotes)
 
 if(process.env.NODE_ENV == "production"){
     //Set Static Folder
-    app.use(express.static('../frontend/build'))
+    app.use(express.static('frontend/build'))
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
