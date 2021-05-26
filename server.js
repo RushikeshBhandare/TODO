714ef5f42c334953dadda9e3397caa470043da6b
RushikeshBhandare <rushi5049@gmail.com>
2021-05-27 01:17:42 +0530

    Change in server file res.sendFile insted of res.send

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if(process.env.NODE_ENV == "production"){
     //Set Static Folder
     app.use(express.static('../frontend/build'))
     app.get('*', (req,res)=>{
-        res.send(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 
